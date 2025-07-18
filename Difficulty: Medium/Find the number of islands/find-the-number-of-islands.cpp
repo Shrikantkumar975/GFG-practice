b1ba7a35eed8356251ab2729195cde59894ef4ba
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2025-07-18 11:51:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -0,0 +1,36 @@
+class Solution {
+  
+  public:
+  
+    void dfs(vector<vector<char>>&grid,int i,int j,int r,int c){
+        if(i==r || j==c || i<0 || j<0 || grid[i][j] == 'W') return;
+        
+        grid[i][j] = 'W';
+        
+        dfs(grid,i,j+1,r,c);
+        dfs(grid,i-1,j+1,r,c);
+        dfs(grid,i-1,j,r,c);
+        dfs(grid,i-1,j-1,r,c);
+        dfs(grid,i,j-1,r,c);
+        dfs(grid,i+1,j-1,r,c);
+        dfs(grid,i+1,j,r,c);
+        dfs(grid,i+1,j+1,r,c);
+    }
+  
+    int countIslands(vector<vector<char>>& grid) {
+        int r = grid.size();
+        int c = grid[0].size();
+        int ans =0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]=='L'){
+                    dfs(grid,i,j,r,c);
+                    ans++;
+                }
+            }
+        }
+        
+        return ans;
+        
+    }
+};
