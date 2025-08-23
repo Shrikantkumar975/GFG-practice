34b6c6cd25cdb09e3aa78e24f9856dfd22535d73
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2025-08-24 03:59:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,40 +1,39 @@
 class Solution {
-public:
-    bool dfs(int node, int parent, vector<vector<int>>& adj, vector<int>& visited) {
-        visited[node] = 1;
-
-        for (int neigh : adj[node]) {
-            if (!visited[neigh]) {
-                if (dfs(neigh, node, adj, visited))
-                    return true;
-            } else if (neigh != parent) {
-                // Visited neighbor that's not the parent => cycle
+  public:
+    bool DFS(vector<vector<int>>&adj,vector<int>&visited,int u,int parent){
+        
+        visited[u]=true;
+        
+        for(int v: adj[u]){
+            if(!visited[v]){
+                if(DFS(adj,visited,v,u)) return true;
+            }
+            else if(v != parent){
                 return true;
             }
         }
-
         return false;
     }
-
+    
     bool isCycle(int V, vector<vector<int>>& edges) {
+        // Code here
         vector<vector<int>> adj(V);
-        for (auto& edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            
             adj[u].push_back(v);
-            adj[v].push_back(u); // since undirected
+            adj[v].push_back(u);
         }
-
-        vector<int> visited(V, 0);
-
-        for (int i = 0; i < V; i++) {
-            if (!visited[i]) {
-                if (dfs(i, -1, adj, visited)) {
-                    return true; // cycle found
-                }
+        
+        vector<int> visited(adj.size(),0);
+        
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+                if(DFS(adj,visited,i,-1)) return true;
             }
         }
-
-        return false; // no cycle
+        
+        return false;
     }
 };
