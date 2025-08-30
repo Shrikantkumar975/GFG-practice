f41520f1b30807bcf52f5fc25a8347ec602fbf9f
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2025-08-30 22:08:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    int celebrity(vector<vector<int>> & mat) {
+    int n = mat.size();
+
+    int i = 0, j = n - 1;
+    while (i < j) {
+        
+        // j knows i, thus j can't be celebrity
+        if (mat[j][i] == 1) 
+            j--;
+
+        // else i can't be celebrity
+        else
+            i++;
+    }
+
+    // i points to our celebrity candidate
+    int c = i;
+
+    // check if c is actually
+    // a celebrity or not
+    for (i = 0; i < n; i++) {
+        if(i == c) continue;
+
+        // if any person doesn't
+        // know 'c' or 'c' doesn't
+        // know any person, return -1
+        if (mat[c][i] || !mat[i][c])
+            return -1;
+    }
+
+    return c;
+}
+};
