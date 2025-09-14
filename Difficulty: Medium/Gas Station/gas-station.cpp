5cab0f48ec2b355ea9bf7e9a59b81086cd022b84
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2025-09-14 22:29:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Gas Station/gas-station.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    
+int startStation(vector<int> &gas, vector<int> &cost) {
+  	int n = gas.size();  
+  
+    // Variables to track total and current remaining gas
+    int totalGas = 0;
+    int currGas = 0;
+    int startIdx = 0;
+
+    // Traverse through each station to calculate remaining
+    // gas in the tank, and total gas 
+    for(int i = 0; i < n; i++) {
+      	currGas += gas[i] - cost[i]; 
+        totalGas += gas[i] - cost[i];  
+
+        // If currGas is negative, circular tour can't 
+      	// start with this index, so update it to next one
+        if(currGas < 0) {
+            currGas = 0;
+            startIdx = i + 1; 
+        }
+    }
+
+    // No solution exist
+    if(totalGas < 0) 
+        return -1;
+
+    return startIdx;
+}
+};
