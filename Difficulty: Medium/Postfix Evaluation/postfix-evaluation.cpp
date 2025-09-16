27f1de173d366a436fed9bbd668ffa64aa310419
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2025-09-17 01:21:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Postfix Evaluation/postfix-evaluation.cpp
+++ Difficulty: Medium/Postfix Evaluation/postfix-evaluation.cpp
@@ -1,34 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
   public:
     
-int floorDiv(int a, int b) {
-    if (a * b < 0 && a % b != 0)
-        return (a / b) - 1;
-    return a / b;
-}
+    void solve(stack<int> &s , string &op){
+        int b = s.top();
+        s.pop();
+        int a = s.top();
+        s.pop();
 
-int evaluatePostfix(vector<string>& arr) {
-    stack<int> st;
+        int temp = 0;
 
-    for (string token : arr) {
-        
-        // If it's an operand (number), push it onto the stack
-        if (isdigit(token[0]) || (token.size() > 1 && token[0] == '-')) {
-            st.push(stoi(token));
-        } 
-        
-        // Otherwise, it must be an operator
-        else {
-            int val1 = st.top(); st.pop(); 
-            int val2 = st.top(); st.pop(); 
-
-            if (token == "+") st.push(val2 + val1);
-            else if (token == "-") st.push(val2 - val1);
-            else if (token == "*") st.push(val2 * val1);
-            else if (token == "/") st.push(floorDiv(val2, val1));
-            else if (token == "^") st.push((int)pow(val2, val1));
+        if(op == "*"){
+            temp = a * b;
         }
+        else if(op == "/"){
+            // floor division (handles negatives properly)
+            temp = (a % b == 0) ? (a / b) : (a / b - ((a < 0) ^ (b < 0)));
+        }
+        else if(op == "+"){
+            temp = a + b;
+        }
+        else if(op == "-"){
+            temp = a - b;
+        }
+        else if(op == "^"){
+            temp = pow(a, b);   // power instead of XOR
+        }
+
+        s.push(temp);
     }
-    return st.top();
-}
+    
+    int evaluatePostfix(vector<string>& arr) {
+        stack<int> s;
+        
+        for(int i = 0; i < arr.size(); i++){
+            if(arr[i] == "*" || arr[i] == "/" || arr[i] == "+" || arr[i] == "-" || arr[i] == "^"){
+                solve(s, arr[i]);
+            } else {
+                s.push(stoi(arr[i]));
+            }
+        }
+        
+        return s.top();
+    }
 };
