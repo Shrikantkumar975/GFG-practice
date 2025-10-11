fd0f831ea642c3b0506a535f99977faaee71a8fe
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2025-10-11 23:39:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum path sum/maximum-path-sum.cpp
@@ -0,0 +1,41 @@
+/*
+class Node {
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    int findMaxSumRec(Node* root, int& res) {
+  
+    if (root == NULL)
+        return 0;
+
+    // Calculate maximum path sums for left and right subtrees
+    int l = max(0, findMaxSumRec(root->left, res));
+    int r = max(0, findMaxSumRec(root->right, res));
+
+    // Update 'res' with the maximum path
+    // sum passing through the current node
+    res = max(res, l + r + root->data);
+
+    return root->data + max(l, r);
+}
+
+// Returns maximum path sum in tree with given root
+int findMaxSum(Node* root) {
+    int res = root->data;
+	
+  	// Compute maximum path sum and store it in 'res'
+    findMaxSumRec(root, res);
+  
+    return res;
+}
+};
