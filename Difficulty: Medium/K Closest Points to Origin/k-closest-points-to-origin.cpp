74b2abbd3cef43ab3131ec9da92736cfe801e3e7
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2025-10-23 20:33:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.cpp
@@ -0,0 +1,51 @@
+class Solution {
+  public:
+    
+// Function to calculate squared distance from the origin
+int squaredDis(vector<int>& point) {
+    return point[0] * point[0] + 
+      point[1] * point[1];
+}
+
+// Function to find k closest points to
+// the origin
+vector<vector<int>> kClosest(
+        vector<vector<int>>& points, int k) {
+    
+    // Max heap to store points with their 
+    // squared distances
+    priority_queue<pair<int, vector<int>>> maxHeap;
+
+    // Iterate through each point
+    for (int i = 0; i < points.size(); i++) {
+        int dist = squaredDis(points[i]);
+
+        if (maxHeap.size() < k) {
+            
+            // If the heap size is less than k, 
+            // insert the point
+            maxHeap.push({dist, points[i]});
+        } else {
+            
+            // If the heap size is k, compare with
+            // the top element
+            if (dist < maxHeap.top().first) {
+                
+                // Replace the top element if the
+                // current point is closer
+                maxHeap.pop();
+                maxHeap.push({dist, points[i]});
+            }
+        }
+    }
+
+    // Take the k closest points from the heap
+    vector<vector<int>> res;
+    while (!maxHeap.empty()) {
+        res.push_back(maxHeap.top().second);
+        maxHeap.pop();
+    }
+
+    return res;
+}
+};
