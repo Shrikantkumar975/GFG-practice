197889683115fa8e183cf662a8d8022cd910ceda
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2025-11-11 22:38:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
+++ Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
@@ -1,24 +1,29 @@
-// User function template for C++
-
 class Solution {
   public:
-    // Function to find length of shortest common supersequence of two strings.
-    int shortestCommonSupersequence(string &s1, string &s2) {
-        // code here
-        int n= s1.size();
-        int m = s2.size();
-        
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s1[i-1] == s2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-            }
+    int minSuperSeq(string &s1, string &s2) {
+    int m = s1.size();
+    int n = s2.size();
+
+    vector<int> prev(n + 1, 0), curr(n + 1, 0);
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            
+            // If chars match
+            if (s1[i - 1] == s2[j - 1])
+                curr[j] = 1 + prev[j - 1];
+                
+            // Else, take max from top or left
+            else
+                curr[j] = max(prev[j], curr[j - 1]);
         }
-        
-        return m+n-dp[n][m];
+        // Move current row to previous
+        prev = curr;
     }
+
+    int lcsLen = prev[n];
+
+    // SCS length = total - common part (LCS)
+    return m + n - lcsLen;
+}
 };
