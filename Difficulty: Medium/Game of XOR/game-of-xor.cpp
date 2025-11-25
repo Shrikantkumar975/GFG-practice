c2be9b33e5a58ef2c32b0dd6704aec036587d423
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2025-11-25 23:12:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Game of XOR/game-of-xor.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int subarrayXor(vector<int>& arr) {
+    int result = 0;
+    int n = arr.size();
+
+    for (int i = 0; i < n; i++) {
+        
+        // Check if (i+1) and (n-i) are both odd
+        if ((i + 1) % 2 != 0 && (n - i) % 2 != 0) {
+            
+            // Include element in final XOR
+            result ^= arr[i];  
+        }
+    }
+
+    return result;
+}
+};
