13bbee92be843607490085ab9974bfa87255c6db
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2025-11-26 23:18:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/AND In Range/and-in-range.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int andInRange(int l, int r) {
+
+    int shiftCount = 0;
+
+    // Iterate through every bit of a and b
+    // simultaneously if a == b then we know that beyond
+    // that the AND value will remain constant
+    while (l != r && l > 0) {
+        shiftCount++;
+        l = l >> 1;
+        r = r >> 1;
+    }
+
+    return (l << shiftCount);
+}
+};
