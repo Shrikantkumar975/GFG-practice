2924f7a2d949227fdfbc992a23f6da3d18103cae
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2025-12-07 23:01:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Number of distinct subsequences/number-of-distinct-subsequences.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int distinctSubseq(string s)
+	{
+	    // Your code goes here
+	    int mod = 1000000007;
+	    int n = s.length();
+	    vector<int>dp(n+1);
+	    dp[0] =1;
+	    
+	    unordered_map<char,int>mp;
+	    for(int i=1; i<=n;i++){
+	        char ch = s[i-1];
+	        dp[i] = (2*dp[i-1])%mod;
+	        if(mp.find(ch)!=mp.end()){
+	            int index = mp[ch];
+	            dp[i] = (dp[i]-dp[index-1]+mod)%mod;
+	        }
+	        mp[ch] =i;
+	    }
+	    return dp[n];
+	}
+};
