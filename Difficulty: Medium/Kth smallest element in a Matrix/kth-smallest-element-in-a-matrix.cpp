44e9630eea9d1d2d43495f831ddb70c6fe41333e
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2025-12-27 22:05:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth smallest element in a Matrix/kth-smallest-element-in-a-matrix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int kthSmallest(vector<vector<int>> &mat, int k) {
+        vector<int> v;
+        
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[0].size();j++){
+                v.push_back(mat[i][j]);
+            }
+        }
+        
+        sort(v.begin(),v.end());
+        
+        return v[k-1];
+    }
+};
