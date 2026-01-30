cbffe9f4b3f89b6de6adea873064dbfc355ae9d4
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-01-30 13:32:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -1,36 +1,36 @@
 class Solution {
-  
   public:
-  
-    void dfs(vector<vector<char>>&grid,int i,int j,int r,int c){
-        if(i==r || j==c || i<0 || j<0 || grid[i][j] == 'W') return;
+    vector<int> dx = {0,1,1,1,0,-1,-1,-1};
+    vector<int> dy = {1,1,0,-1,-1,-1,0,1};
+    void dfs(vector<vector<char>> &grid,int i,int j){
+        int n=grid.size();
+        int m=grid[0].size();
         
-        grid[i][j] = 'W';
+        if(i>=n || i<0 || j>=m || j<0 || grid[i][j] == 'W') return ;
         
-        dfs(grid,i,j+1,r,c);
-        dfs(grid,i-1,j+1,r,c);
-        dfs(grid,i-1,j,r,c);
-        dfs(grid,i-1,j-1,r,c);
-        dfs(grid,i,j-1,r,c);
-        dfs(grid,i+1,j-1,r,c);
-        dfs(grid,i+1,j,r,c);
-        dfs(grid,i+1,j+1,r,c);
+        grid[i][j]='W';
+        
+        for(int k=0;k<8;k++)
+            dfs(grid,i+dx[k],j+dy[k]);
     }
   
     int countIslands(vector<vector<char>>& grid) {
-        int r = grid.size();
-        int c = grid[0].size();
-        int ans =0;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
+        // Code here
+        int n=grid.size();
+        int m=grid[0].size();
+        int count =0;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]=='L'){
-                    dfs(grid,i,j,r,c);
-                    ans++;
+                    dfs(grid,i,j);
+                    count++;
                 }
             }
         }
         
-        return ans;
+        
+        return count;
         
     }
 };
