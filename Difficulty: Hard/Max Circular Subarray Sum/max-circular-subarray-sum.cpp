46b16f337c1121cb5159f0fd2d2d4b055e0e39f4
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-02-02 23:01:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.cpp
+++ Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.cpp
@@ -1,33 +1,27 @@
 class Solution {
   public:
     int maxCircularSum(vector<int> &arr) {
-        // code here
         int totalSum = 0;
-    int currMaxSum = 0, currMinSum = 0;
-    int maxSum = arr[0], minSum = arr[0];
-
-    for (int i = 0; i < arr.size(); i++) {
-
-        // Kadane's to find maximum sum subarray
-        currMaxSum = max(currMaxSum + arr[i], arr[i]);
-        maxSum = max(maxSum, currMaxSum);
-
-        // Kadane's to find minimum sum subarray
-        currMinSum = min(currMinSum + arr[i], arr[i]);
-        minSum = min(minSum, currMinSum);
-
-        // Sum of all the elements of input array
-        totalSum = totalSum + arr[i];
-    }
-
-    int normalSum = maxSum;
-    int circularSum = totalSum - minSum;
-
-    // If the minimum subarray is equal to total Sum
-    // then we just need to return normalSum
-    if (minSum == totalSum)
-        return normalSum;
-
-    return max(normalSum, circularSum);
+        
+        int maxSum = arr[0], currMax = arr[0];
+        int minSum = arr[0], currMin = arr[0];
+        
+        for (int i = 0; i < arr.size(); i++) {
+            totalSum += arr[i];
+            
+            if (i > 0) {
+                currMax = max(arr[i], currMax + arr[i]);
+                maxSum = max(maxSum, currMax);
+                
+                currMin = min(arr[i], currMin + arr[i]);
+                minSum = min(minSum, currMin);
+            }
+        }
+        
+        // If all elements are negative
+        if (maxSum < 0)
+            return maxSum;
+        
+        return max(maxSum, totalSum - minSum);
     }
 };
