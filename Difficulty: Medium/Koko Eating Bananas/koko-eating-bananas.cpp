5cf2347203db6315f5b0961fb95011031965d54f
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-02-10 23:02:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.cpp
+++ Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.cpp
@@ -1,31 +1,30 @@
 class Solution {
   public:
+  
     int check(vector<int> &arr,int mid){
-        double curr=0;
+        int count =0;
         
         for(int &a: arr){
-            curr+=ceil(a*1.0/mid);
+            count+=ceil(a*1.0/mid);
         }
         
-        return curr;
-        
+        return count;
     }
-  
+    
     int kokoEat(vector<int>& arr, int k) {
         // Code here
         int low = 1;
         int high = 1e6;
-        
         int ans=0;
         
         while(low<=high){
-            int mid =  low + (high - low)/2;
+            int mid =  low + (high - low )/2;
             
             if(check(arr,mid) <= k){
-                ans=mid;
-                high=mid-1;
+                ans = mid;
+                high = mid -1;
             }else{
-                low=mid+1;
+                low = mid +1;
             }
         }
         
