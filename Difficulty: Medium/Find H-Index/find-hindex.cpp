c10e3dfa897669106319dbf0d8a67b971e45bb92
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-02-21 22:45:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.cpp
+++ Difficulty: Medium/Find H-Index/find-hindex.cpp
@@ -1,30 +1,15 @@
 class Solution {
   public:
-    int hIndex(vector<int> &citations) {
-
-    int n = citations.size();
-    vector<int> freq(n + 1);
-
-    // count the frequency of citations
-    for (int i = 0; i < n; i++) {
-        if (citations[i] >= n)
-            freq[n] += 1;
-        else
-            freq[citations[i]] += 1;
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end(), greater<int>());
+        
+        int h = 0;
+        for(int i = 0; i < citations.size(); i++){
+            if(citations[i] >= i + 1)
+                h = i + 1;
+            else
+                break;
+        }
+        return h;
     }
-
-    int idx = n;
-    
-    // variable to keep track of the count of papers
-    // having at least idx citations
-    int s = freq[n]; 
-    while (s < idx) {
-        idx--;
-        s += freq[idx];
-    }
-  
-    // return the largest index for which the count of 
-    // papers with at least idx citations becomes >= idx
-    return idx;
-}
 };
