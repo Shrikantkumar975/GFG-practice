abb91d69ae1eee2d8c7884ba6847283f09aa06b9
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-03-03 11:29:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest subarray with Atmost two distinct integers/longest-subarray-with-atmost-two-distinct-integers.cpp
+++ Difficulty: Medium/Longest subarray with Atmost two distinct integers/longest-subarray-with-atmost-two-distinct-integers.cpp
@@ -1,31 +1,26 @@
 class Solution {
-public:
-    int totalElements(vector<int>& arr) {
-        int n = arr.size();
-        unordered_map<int, int> mp;
-
-        int i      = 0;          // left boundary of window
-        int j      = 0;          // right boundary of window
-        int result = 0;          // best window size seen
-
-        while (j < n) {
-            // include arr[j] into the window
-            mp[arr[j]]++;
-
-            // if more than two distinct numbers, shrink from left
-            while (mp.size() > 2) {
-                mp[arr[i]]--;            // decrement freq of arr[i]
-                if (mp[arr[i]] == 0) {   // if freq is zero, remove key
-                    mp.erase(arr[i]);
-                }
-                i++;                     // move left boundary right
+  public:
+    int totalElements(vector<int> &arr) {
+        // code here
+        int i=0;
+        int j=0;
+        unordered_set<int> st;
+        unordered_map<int,int> mpp;
+        int maxi=0;
+        
+        while(j<arr.size()){
+            mpp[arr[j]]++;
+            st.insert(arr[j]);
+            if(st.size()<=2){
+                maxi = max(maxi,j-i+1);
+            }else if(st.size()>2){
+                mpp[arr[i]]--;
+                if(mpp[arr[i]]==0) st.erase(arr[i]);
+                i++;
             }
-
-            // window [i..j] is valid, update result
-            result = max(result, j - i + 1);
-            j++;                        // expand window to the right
+            j++;
         }
-
-        return result;
+        
+        return maxi;
     }
 };
