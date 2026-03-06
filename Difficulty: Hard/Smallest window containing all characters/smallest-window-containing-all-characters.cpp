3d70dbd8221b9358f6fa335ad44cc9d21c10fbdd
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-03-06 23:55:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.cpp
+++ Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.cpp
@@ -1,57 +1,45 @@
 class Solution {
-  public:
-    string smallestWindow(string s, string p){
-    int len1 = s.length();
-    int len2 = p.length();
-
-    if (len1 < len2)
-        return "";
-
-    vector<int> countP(256, 0);
-    vector<int> countS(256, 0);
-
-    // Store occurrence of characters of P
-    for (int i = 0; i < len2; i++)
-        countP[p[i]]++;
-
-    int start = 0, start_idx = -1, min_len = INT_MAX;
-
-    int count = 0;
-
-    for (int j = 0; j < len1; j++){
-        // Count occurrence of characters 
-        // of string S
-        countS[s[j]]++;
-
-        // If S's char matches with P's char,
-        // increment count
-        if (countP[s[j]] != 0 && countS[s[j]] <= countP[s[j]]){
-            count++;
-        }
-
-        // If all characters are matched
-        if (count == len2){
-            // Try to minimize the window
-            while (countS[s[start]] > countP[s[start]] || 
-                   countP[s[start]] == 0){
-                if (countS[s[start]] > countP[s[start]]){
-                    countS[s[start]]--;
+public:
+    string minWindow(string &s, string &p) {
+        
+        vector<int> need(26,0);
+        
+        for(char c : p)
+            need[c-'a']++;
+        
+        int left = 0;
+        int count = p.size();
+        
+        int start = 0;
+        int minLen = INT_MAX;
+        
+        for(int right = 0; right < s.size(); right++)
+        {
+            if(need[s[right]-'a'] > 0)
+                count--;
+                
+            need[s[right]-'a']--;
+            
+            while(count == 0)
+            {
+                if(right - left + 1 < minLen)
+                {
+                    minLen = right - left + 1;
+                    start = left;
                 }
-                start++;
-            }
-
-            // Update window size
-            int len = j - start + 1;
-            if (min_len > len){
-                min_len = len;
-                start_idx = start;
+                
+                need[s[left]-'a']++;
+                
+                if(need[s[left]-'a'] > 0)
+                    count++;
+                    
+                left++;
             }
         }
+        
+        if(minLen == INT_MAX)
+            return "";
+            
+        return s.substr(start, minLen);
     }
-
-    if (start_idx == -1)
-        return "";
-
-    return s.substr(start_idx, min_len);
-}
 };
