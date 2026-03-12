3504c36c94bc927acc9206bca886191eab90334e
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-03-12 23:29:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimum K Consecutive Bit Flips/minimum-k-consecutive-bit-flips.cpp
+++ Difficulty: Hard/Minimum K Consecutive Bit Flips/minimum-k-consecutive-bit-flips.cpp
@@ -1,43 +1,28 @@
 class Solution {
-  public:
-    int kBitFlips(vector<int> &arr, int k) {
-    int n = arr.size(); 
-    int res = 0, flag = 0;
-    queue<int> q; 
-
-    for (int i = 0; i < n; i++) {
+public:
+    int kBitFlips(vector<int>& arr, int k) {
+        int n = arr.size();
+        vector<int> hint(n,0);
         
-        if(i >= k) {
-            flag ^= q.front();
-            q.pop();
+        int flip = 0;
+        int ans = 0;
+        
+        for(int i=0;i<n;i++){
+            
+            if(i >= k)
+                flip ^= hint[i-k];
+            
+            if((arr[i] ^ flip) == 0){
+                
+                if(i + k > n)
+                    return -1;
+                
+                hint[i] = 1;
+                flip ^= 1;
+                ans++;
+            }
         }
         
-        // If flag = 1, then flip the current index
-        if(flag == 1)
-            arr[i] ^= 1;
-        
-        // Finally, if arr[i] = 0, then we need to flip
-        if(arr[i] == 0) {
-            
-            // Check if k elements are left
-            if(i + k > n) 
-                return -1;
-            
-            res += 1;
-            
-            // Flip flag so that upcoming elements are also flipped
-            flag ^= 1;
-            
-            // If we flip, push 1 to the queue
-            q.push(1);
-        }
-        else {
-            
-            // If we don't flip, push 0 to queue
-            q.push(0);
-        }
+        return ans;
     }
-
-    return res; 
-}
 };
