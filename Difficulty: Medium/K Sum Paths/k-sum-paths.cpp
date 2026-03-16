4dc580dc1ff80643f4b8be3ac39386b5940b1b88
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-03-16 23:06:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K Sum Paths/k-sum-paths.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+  
+    void dfs(Node* root, long long currSum, int k, 
+             unordered_map<long long,int> &mp, int &count){
+        
+        if(root == NULL) return;
+        
+        currSum += root->data;
+        
+        if(mp.find(currSum - k) != mp.end())
+            count += mp[currSum - k];
+        
+        mp[currSum]++;
+        
+        dfs(root->left, currSum, k, mp, count);
+        dfs(root->right, currSum, k, mp, count);
+        
+        mp[currSum]--;
+    }
+  
+    int countAllPaths(Node *root, int k) {
+        
+        unordered_map<long long,int> mp;
+        mp[0] = 1;
+        
+        int count = 0;
+        
+        dfs(root, 0, k, mp, count);
+        
+        return count;
+    }
+};
