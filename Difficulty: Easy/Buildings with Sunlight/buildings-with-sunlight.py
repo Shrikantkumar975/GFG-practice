b2780fbd70cafe56a6706e4c9a118e06c9bfe6de
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-04-24 23:20:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Buildings with Sunlight/buildings-with-sunlight.py
@@ -0,0 +1,11 @@
+class Solution:
+    def visibleBuildings(self, arr):
+        maxi=arr[0]
+        count=1
+        
+        for i in range(1,len(arr)):
+            if arr[i] >= maxi:
+                count+=1
+                maxi = arr[i]
+    
+        return count        
