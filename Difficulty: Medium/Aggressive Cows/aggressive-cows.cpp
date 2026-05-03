eac2e93eea6516e97620d8a53f5aab4d1ae7da21
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-05-03 15:26:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
@@ -1,38 +1,41 @@
-// User function Template for C++
-
 class Solution {
   public:
-  
-  bool canWe(vector<int>&stalls,int mid,int k){
-      int count=1;
-      int last = stalls[0];
-      for(int i=1;i<stalls.size();i++){
-          if(stalls[i]-last>=mid){
-              count++;
-              last=stalls[i];
-          }
-          if(count>=k) return true;
-      }
-      return false;
-  }
-
-    int aggressiveCows(vector<int> &stalls, int k) {
-        int n = stalls.size();
-        sort(stalls.begin(),stalls.end());
-        int low = 1,high = stalls[n-1]-stalls[0];
+    
+    int canPlace(vector<int> &stalls,int cow,int dist){
+        int cowCount = 1;
+        int last = stalls[0];
         
-        int ans = -1;
+        for(int i=1;i<stalls.size();i++){
+            if(stalls[i]-last >= dist){
+                cowCount++;
+                last =  stalls[i];
+            }
+            if(cowCount >= cow) return true;
+        }
+        return false;
+    }
+    
+  
+    int aggressiveCows(vector<int> &stalls, int k) {
+        // code here
+        sort(stalls.begin(),stalls.end());
+        int n = stalls.size();
+        int ans=-1;
+        
+        int low = 1;
+        int high = stalls[n-1]-stalls[0];
         
         while(low<=high){
-            int mid = (low+high)/2;
+            int mid = (low + high)/2;
             
-            if(canWe(stalls,mid,k)){
-                ans= mid;
-                low= mid+1;
+            if(canPlace(stalls,k,mid)){
+                ans = mid;
+                low = mid+1;
             }else{
                 high = mid-1;
             }
         }
+        
         return ans;
     }
 };
