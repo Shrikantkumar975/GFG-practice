1196dfaeeeaa18775bea03386f56555264accfe1
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-08-28 03:04:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Largest Rectangle with Column Swaps/largest-rectangle-with-column-swaps.cpp
+++ Difficulty: Hard/Largest Rectangle with Column Swaps/largest-rectangle-with-column-swaps.cpp
@@ -1,36 +1,55 @@
 class Solution {
   public:
-    int maxArea(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
-
-        vector<int> height(m, 0);
-        int ans = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            // Update heights for current row
-            for (int j = 0; j < m; j++) {
-                if (mat[i][j] == 1)
-                    height[j]++;
-                else
-                    height[j] = 0;
+    
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int n = heights.size();
+        int res=0;
+    
+        for(int i=0;i<heights.size();i++){
+            while(!st.empty() && heights[st.top()]>heights[i]){
+                int currPop = st.top();
+                st.pop();
+    
+                int pse = st.empty()?-1:st.top();
+    
+                res = max(res,heights[currPop]*(i-pse-1));
             }
-
-            // IMPORTANT: don't sort height directly
-            vector<int> temp = height;
-
-            // Sort heights in descending order
-            sort(temp.begin(), temp.end(), greater<int>());
-
-            // Calculate maximum rectangle
-            for (int j = 0; j < m; j++) {
-                int width = j + 1;
-                int area = temp[j] * width;
-                ans = max(ans, area);
-            }
+            st.push(i);
         }
-
-        return ans;
+    
+        while(!st.empty()){
+            int currPop = st.top();
+            st.pop();
+    
+            int pse = st.empty()?-1:st.top();
+    
+            res = max(res,heights[currPop]*(n-pse-1));
+        }
+    
+        return res;
+    }
+    
+    int maxArea(vector<vector<int>>& mat) {
+       vector<int> m(mat[0].size(),0);
+       int maxi=0;
+       
+       for(int i=0;i<mat.size();i++){
+           
+           for(int j=0;j<mat[0].size();j++){
+               if(mat[i][j]==0)
+                    m[j]=0;
+                else{
+                    m[j]++;
+                }
+           }
+            
+            vector<int> temp = m;
+            
+            sort(temp.begin(),temp.end());
+            
+            maxi= max(maxi,largestRectangleArea(temp));
+       }
+       return maxi;
     }
 };
