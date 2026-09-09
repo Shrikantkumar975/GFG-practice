c817d10f1fa3fdd36f67a3bfc955e05dd07314c8
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-09-10 02:08:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.cpp
+++ Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.cpp
@@ -2,6 +2,14 @@ class Solution {
   public:
     void segregate0and1(vector<int> &arr) {
         // code here
-        sort(arr.begin(),arr.end());
+        int low = 0;
+        int high = arr.size()-1;
+        
+        while(low < high){
+            if(arr[low]==1){
+                swap(arr[low],arr[high]);
+                high--;
+            }else low++;
+        }
     }
 };
